2893a5e12923cf2afe46407a678ad20102bdb53b
phpjibo-prog <phpjibo@gmail.com>
2025-12-13 13:49:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ DB_CONFIG = {
     'host': os.environ.get('MYSQL_HOST'),
     'user': os.environ.get('MYSQL_USER'),
     'password': os.environ.get('MYSQL_PASSWORD'),
-    'database': os.environ.get('MYSQL_DATABASE') 
+    'database': os.environ.get('MYSQL_DATABASE'), 
     'port': os.environ.get('MYSQL_PORT', 3306)
 }
 
