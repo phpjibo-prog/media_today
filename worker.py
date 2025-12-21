53de9b18498030abb9df028b81a0f9392debd221
phpjibo-prog <phpjibo@gmail.com>
2025-12-22 00:28:00 +0300

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,17 +1,1 @@
-import os
-from redis import Redis
-from rq import Worker, Queue
-
-# Get the Redis URL from environment
-redis_url = os.environ.get('REDIS_URL')
-if not redis_url:
-    raise RuntimeError("REDIS_URL environment variable not set")
-
-redis_conn = Redis.from_url(redis_url)
-
-listen = ['default']
-
-if __name__ == '__main__':
-    queues = [Queue(name, connection=redis_conn) for name in listen]
-    worker = Worker(queues)
-    worker.work()
+print("hello")
