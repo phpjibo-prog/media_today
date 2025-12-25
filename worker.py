0ddcd4ca601619a3011ae0f775b4009f215ab193
phpjibo-prog <phpjibo@gmail.com>
2025-12-25 11:40:08 +0300

    Update worker.py

--- worker.py
+++ worker.py
@@ -31,7 +31,7 @@ def main():
     )
 
     # Start the recorder in a separate thread/background
-    recorder.start()
+    #recorder.start()
 
     temp_folder = os.path.join(os.getcwd(), 'temp')
     os.makedirs(temp_folder, exist_ok=True)
